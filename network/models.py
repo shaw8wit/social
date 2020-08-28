c38d6e4f7f49c706cb249e8b7a178da0e311e4b7
shaw8wit <shaw8wit@gmail.com>
2020-08-28 13:19:30 +0530

    Initialization

---
+++ network/models.py
@@ -0,0 +1,22 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    date = models.DateTimeField()
+    likes = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.id} : {self.user} posts -> {self.content} on {self.date} , {self.likes} likes"
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.CharField(max_length=500)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
